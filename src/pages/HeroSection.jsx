d576355ad3b0ca9ff2a41dbdbd2d95930c030192
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-03-29 20:09:44 +0000

    intro text padding

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -5,8 +5,13 @@ const HeroSection = () => {
   return (
   <div className='container'>
    <div className="intro-text">
-    <h5>Hello! I'm</h5>
-    <h1>Emmanuel Nyatepe</h1>
+    <h5 className="hello-text">Hello! I'm</h5>
+    <h1 className="name">Emmanuel E. Nyatepe</h1>
+    <h2>Full Stack Developer</h2>
+    <button className="btn-git">GET IN TOUCH</button>
+    <div className="socials">
+
+    </div>
    </div>
    <div className="profile"></div>
   </div>)
