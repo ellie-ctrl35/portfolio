b971baa28be52ff4ddc1003dcdc3b6a2fbcf3758
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-03-29 21:47:57 +0000

    NeonCard Component

---
+++ src/components/NeonCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NeonCard.css'; // Make sure to create a NeonCard.css file for styles
+
+const NeonCard = ({ Icon, title, description, gradientColors, shadowColors }) => {
+  const backgroundStyle = {
+    background: `linear-gradient(${gradientColors.join(', ')})`,
+    boxShadow: shadowColors.map(color => `0 0 10px ${color}`).join(', '),
+  };
+
+  return (
+    <div className="neon-card" style={backgroundStyle}>
+      {Icon && <Icon className="neon-card-icon" />}
+      <h3 className="neon-card-title">{title}</h3>
+      <p className="neon-card-description">{description}</p>
+    </div>
+  );
+};
+
+export default NeonCard;
--- src/pages/ExperienceSection.jsx
+++ src/pages/ExperienceSection.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Services.css'
 
 const ExperienceSection = () => {
   return (
-    <div>ExperienceSection</div>
+    <div className='container'>
+     <div className='skill-container'>
+
+     </div>
+     <div className="skill-text">
+      
+     </div>
+    </div>
   )
 }
 
