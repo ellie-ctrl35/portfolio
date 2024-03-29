d35869a81f48b838cec4e09acbedf28af6f73e52
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-03-29 20:38:01 +0000

    icons for socials

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import  './Hero.css'
+import { Home01Icon } from '@hugeicons/react-pro';
 
 const HeroSection = () => {
   return (
@@ -10,10 +11,12 @@ const HeroSection = () => {
     <h2>Full Stack Developer</h2>
     <button className="btn-git">GET IN TOUCH</button>
     <div className="socials">
-
+     <Home01Icon size={32} color="white"/>
     </div>
    </div>
-   <div className="profile"></div>
+   <div className="profile">
+    
+   </div>
   </div>)
 };
 
