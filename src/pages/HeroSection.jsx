605bd6d9519948840b5a380b783587f82aab40cb
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-03-29 20:42:14 +0000

    material ui icons

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import  './Hero.css'
-import { Home01Icon } from '@hugeicons/react-pro';
+import { GitHubIcon} from '@mui/icons-material';
+
 
 const HeroSection = () => {
   return (
@@ -11,7 +12,7 @@ const HeroSection = () => {
     <h2>Full Stack Developer</h2>
     <button className="btn-git">GET IN TOUCH</button>
     <div className="socials">
-     <Home01Icon size={32} color="white"/>
+     <GitHubIcon size={32} color="white"/>
     </div>
    </div>
    <div className="profile">
