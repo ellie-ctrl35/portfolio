77f90705c7996176efc608289084292faee47a76
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-03-29 18:10:19 +0000

    container adjustment

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import styles from './Hero.module.css'
 
 const HeroSection = () => {
-  return (
-    <div>HeroSection</div>
-  )
-}
+  return 
+  <div className={styles.container}>
+    HeroSection
+  </div>;
+};
 
-export default HeroSection
+export default HeroSection;
