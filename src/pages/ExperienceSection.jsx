4909863f350a9ef7951dfe22f830884fccc0529e
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-03-30 09:07:31 +0000

    skill card

--- src/pages/ExperienceSection.jsx
+++ src/pages/ExperienceSection.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import './Services.css'
+import { HiGlobeAlt } from "react-icons/hi";
+import NeonCard from '../components/NeonCard';
 
 const ExperienceSection = () => {
   return (
     <div className='container'>
      <div className='skill-container'>
-
+     <NeonCard
+        Icon={HiGlobeAlt} // Replace with your chosen icon component
+        title="Web App"
+        description="Lorem ipsum dolor sit amet..."
+        gradientColors={['#7928CA', '#FF0080']} // Your gradient colors
+        shadowColors={['#7928CA', '#FF0080']} // Your shadow colors
+      />
      </div>
      <div className="skill-text">
-      
+
      </div>
     </div>
   )
