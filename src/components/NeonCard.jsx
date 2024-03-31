50bc12d01c3c839fdce7d5e7032f78f2f3dde073
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-03-31 17:59:46 +0000

    glassmorphism

--- src/components/NeonCard.jsx
+++ src/components/NeonCard.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './NeonCard.css'; // Make sure to create a NeonCard.css file for styles
 
-const NeonCard = ({ Icon, title, description, gradientColors }) => {
-  const backgroundStyle = {
-    background: `linear-gradient(${gradientColors.join(', ')})`,
-  };
+const NeonCard = ({ Icon, title, description }) => {
+  
 
   return (
-    <div className="neon-card" style={backgroundStyle}>
+    <div className="neon-card" >
       <div className='header-txt'>
         {Icon && <Icon className="neon-card-icon" />}
         <h3 className="neon-card-title">{title}</h3>
