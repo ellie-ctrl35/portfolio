19967c91226f1be64faad0fea009db40db0b620c
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-04-01 00:41:14 +0000

    About section biography done

--- src/pages/AboutSection.jsx
+++ src/pages/AboutSection.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './About.css'
 
 const AboutSection = () => {
   return (
-    <div>AboutSection</div>
+    <div className='container'>
+      <div className="about-container">
+       <div className='header-title'>
+        <div className='dash'></div>
+        <h3>About Me</h3>
+       </div>
+       <p>I'm a passionate Full Stack Developer with a knack for creating fluid digital experiences. With a solid grasp of front-end and back-end technologies, I bring ideas to life in the digital space. When I'm not coding, you'll find me exploring the latest in technology or contributing to open source.</p>
+        <p>Driven by curiosity and the thrill of problem-solving, I'm continually learning and expanding my skill set to stay at the forefront of industry trends. Let's connect and make something amazing together!</p>
+      <p>I'm in my Final Semster studying Computer Science at Valley View University <br />It's been a very smoooth journey here. <br />I'm really interested I building more Augmented Reality Applications beacuse I believe aside AI ,Augmented reality is the future of software engineering  </p>
+      </div>
+      <div className="stack-container">
+      <div className='header-title'>
+        <div className='dash'></div>
+        <h3>My Stack</h3>
+      </div>
+      
+      </div>
+    </div>
   )
 }
 
