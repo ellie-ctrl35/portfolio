203669e23f1a238c0bb2e7a89f73b5085196721c
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-04-04 14:22:12 +0000

    hover top

--- src/pages/ProjectSection.jsx
+++ src/pages/ProjectSection.jsx
@@ -1,30 +1,42 @@
-import {useState} from 'react'
-import './Projects.css'
+import { useState } from "react";
+import "./Projects.css";
 
 const ProjectSection = () => {
-  const [selectedTab, setSelectedTab] = useState('web');
+  const [selectedTab, setSelectedTab] = useState("web");
   const handleTabChange = (tab) => {
     setSelectedTab(tab);
   };
   return (
-    <div className='container2'>
-      <nav className='project-nav'>
+    <div className="container2">
+      <nav className="project-nav">
         <div className="project-header">
           <div className="dash"></div>
           <h1>Projects</h1>
         </div>
-        
-        <ul>
-          <li onClick={() => handleTabChange('web')}>Web</li>
-          <li onClick={() => handleTabChange('mobile')}>Mobile</li>
-          <li onClick={() => handleTabChange('desktop')}>Desktop</li>
+        <ul className="project-ul">
+          <li
+            onClick={() => handleTabChange("web")}
+            selected={selectedTab === "web" ? "true" : "false"}
+          >
+            Web
+          </li>
+          <li
+            onClick={() => handleTabChange("mobile")}
+            selected={selectedTab === "mobile" ? "true" : "false"}
+          >
+            Mobile
+          </li>
+          <li
+            onClick={() => handleTabChange("desktop")}
+            selected={selectedTab === "desktop" ? "true" : "false"}
+          >
+            Desktop
+          </li>
         </ul>
       </nav>
-      <div className="project-container">
-
-      </div>
+      <div className="project-container"></div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectSection
+export default ProjectSection;
